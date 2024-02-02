1f386c4cc72680bd0b44ddaa65899874b1bac2f7
MiQieR <996luoxuhe@gmail.com>
2024-02-03 03:15:29 +0800

    Site updated: 2024-02-03 03:15:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["ASUS-Router-Light-NAS/","Chainedbox-OMV/","Chainedbox-OpenWrt/","DIY-x86-NAS/","Dell-XPS9360-Hackintosh/","External-Hard-Drive-Case/","Free-Domain-&-CDN-for-Blog/","Hexo-Blog/","Markdown-Advance-Format/","NAS-Aria2-Downloader/","Pocket-WiFi-with-Snapdragon-410/","Solid-state-Drive(SSD)/","iOS-Sideloading-AltStore/"];function toRandomPost(){
+var posts=["ASUS-Router-Light-NAS/","Chainedbox-OMV/","Chainedbox-OpenWrt/","Dell-XPS9360-Hackintosh/","DIY-x86-NAS/","External-Hard-Drive-Case/","Free-Domain-&-CDN-for-Blog/","Hexo-Blog/","Markdown-Advance-Format/","NAS-Aria2-Downloader/","Pocket-WiFi-with-Snapdragon-410/","Solid-state-Drive(SSD)/","iOS-Sideloading-AltStore/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
